d6c6f9b223576dc0b05bba46077da46f6874189c
Matt Turland <matt.turland@stacksports.com>
2020-05-02 22:59:21 -0500

    Add local server to CORS allowed origins

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -65,7 +65,7 @@ class AppServiceProvider implements ServiceProviderInterface
         $app->add(function ($request, $handler) {
             $response = $handler->handle($request);
             return $response
-                ->withHeader('Access-Control-Allow-Origin', 'https://matthewturland.com')
+                ->withHeader('Access-Control-Allow-Origin', 'https://matthewturland.com, http://localhost:8080')
                 ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
                 ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         });
